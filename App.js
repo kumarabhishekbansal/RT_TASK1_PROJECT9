9156bf257757a1ea6e2ef817c1ba28136f04beaa
kumarabhishekbansal <kumarabhishekbansal>
2024-03-29 14:30:59 +0530

    initial setup and setting up bottom navigation bar

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { FontAwesome } from "@expo/vector-icons";
+import React from "react";
+import {
+  NavigationContainer,
+  getFocusedRouteNameFromRoute,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Posts from "./screens/Posts";
+import PostDetail from "./screens/PostDetail";
+import Profile from "./screens/Profile";
+import Login from "./screens/Login";
+import PostForm from "./screens/PostForm";
+import AppContext from "./context/AppContext";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
-export default function App() {
+function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: "blue",
+        tabBarInactiveTintColor: "gray",
+        tabBarIcon: ({ color, size }) => {
+          const iconName =
+            (route.name === "Posts" && "feed") ||
+            (route.name === "PostForm" && "plus-square") ||
+            (route.name === "Profile" && "user");
+          return <FontAwesome name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Stack.Screen
+        name="Posts"
+        component={Posts}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Profile"
+        component={Profile}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="PostForm"
+        component={PostForm}
+        options={{ headerShown: false, tabBarLabel: "Add post" }}
+      />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <AppContext>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={({ route }) => ({
+              headerTitle: getFocusedRouteNameFromRoute(route),
+            })}
+          />
+          <Stack.Screen name="PostDetail" component={PostDetail} />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AppContext>
+  );
+}
---
+++ app.config.js
@@ -0,0 +1,5 @@
+export default {
+    extra: {
+      apiUrl: `http://192.168.1.7:3000`,
+    },
+  };
